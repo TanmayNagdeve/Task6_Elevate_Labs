57e4726c74a8c6652d13dca5812a74ab4909b5f3
Tanmay Nagdeve <55934827+TanmayNagdeve@users.noreply.github.com>
2025-05-22 21:20:54 +0530

    Add files via upload

--- day6.sql
+++ day6.sql
@@ -12,13 +12,14 @@ SELECT `Date`,
 		month(Date)
         from onlinesalesdata;
 
--- QUERY TO GET TOTAL REVENUE LIMITING TO SPECIFIC TIME PERIODS, ORDERED IN DESCENDING
+-- QUERY TO GET TOTAL REVENUE LIMITING TO SPECIFIC TIME PERIODS, ORDERED IN DESCENDING FOR THE TOP 3 MONTHS
 SELECT sum(`Total Revenue`) as Total_Revenue,
     month(Date) as Month 
     from onlinesalesdata
-    where `Date` between '2024-01-15 00:00:00' and '2024-06-15 00:00:00'
+    where `Date` between '2024-01-01 00:00:00' and '2024-08-27 00:00:00'
     group by `Month`
-	order by `Total_Revenue` desc;
+	order by `Total_Revenue` desc
+    Limit 3;
      
 -- QUERY TO GET TRANSACTION COUNT FOR EACH PRODUCT CATEGORY 
 SELECT count(distinct(`Transaction ID`)),
